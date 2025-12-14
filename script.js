29b05518975f86392ebc8b7a22e04283ad921553
lakshmi-nath09 <24881A0528@student.vardhaman.org>
2025-12-14 12:01:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function scrollToSection(id) {
     behavior: "smooth"
   });
 }
+// Dark Mode Toggle
+const darkToggle = document.getElementById("darkToggle");
+
+darkToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
